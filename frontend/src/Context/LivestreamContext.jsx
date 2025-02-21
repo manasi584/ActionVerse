a59c60f6f47c68a6aee641c35afec65b8fb1f1f1
manasi584 <120204669+manasi584@users.noreply.github.com>
2025-02-22 01:05:13 +0530

    Update LivestreamContext.jsx

--- frontend/src/Context/LivestreamContext.jsx
+++ frontend/src/Context/LivestreamContext.jsx
@@ -1,25 +1,26 @@
 export const sampleLiveStreams = [
-      {
-        id: 1,
-        artist: "DJ Vibe",
-        title: "Live Street DJ Performance",
-        viewers: 150,
-        thumbnail:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGWvmcSHtqqTxCERRSgsCKihr-RkolvJ640g&s",
-      },
-      {
-        id: 2,
-        artist: "Dance Crew X",
-        title: "Freestyle Dance Battle",
-        viewers: 220,
-        thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2xyEHHYE-HPoxU99ugf9il6TW7EnV4DH09Q&s",
-      },
-      {
-        id: 3,
-        artist: "Poet Maya",
-        title: "Spoken Word Poetry Night",
-        viewers: 90,
-        thumbnail:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQQeIenb_mpCi0MDlJXz_U1qcn92OY4x69Dg&s",
-      },
-    ];
+  {
+    id: 1,
+    artist: "Climate Warrior",
+    title: "Live Reforestation Drive",
+    viewers: 300,
+    thumbnail:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPl_oYRyTb3E0dYxnifUt0IwlEz__INDO2Ng&s",
+  },
+  {
+    id: 2,
+    artist: "Voices for Justice",
+    title: "Human Rights Awareness Rally",
+    viewers: 450,
+    thumbnail:
+      "https://cdn.corporatefinanceinstitute.com/assets/social-justice.jpeg",
+  },
+  {
+    id: 3,
+    artist: "Poet Maya",
+    title: "Spoken Word for Social Change",
+    viewers: 180,
+    thumbnail:
+      "https://media.licdn.com/dms/image/v2/D4D12AQFlAwLR7_tHEQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1677002645405?e=2147483647&v=beta&t=HaiJk-VdX2Q_fGBgzpZDWqOo5S5DzeVowvvt8jw3mKY",
+  },
+];
